6d964fb7c551f09d12d8fe3307bd533ccfdd2180
miqbalnawawi <75749294+miqbalnawawi@users.noreply.github.com>
2021-01-12 05:43:53 +0700

    Update script-automation-webserver.sh

--- script-automation-webserver.sh
+++ script-automation-webserver.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+echo "============update repo================"
 #update 
 sudo apt-get update -y
 
-#install package nginx , mysql dan php
-sudo apt-get install -y nginx php-mysqli mysql-server php-fpm git
+echo "===========install package nginx , mysql , php , git =================="
+#install package nginx , mysql , php dan git
+sudo apt-get install -y nginx php-mysqli mysql-server php-fpm git	
 
-#create file pesbuk
+echo "===================create config nginx file pesbuk==============="
+#create config nginx file pesbuk
 sudo tee /etc/nginx/sites-available/pesbuk <<EOF
 server {
+        
         listen 80;
         root /var/www/html;
         # Add index.php to the list if you are using PHP
         index index.php index.html index.htm index.nginx-debian.html;
         server_name localhost;
         location / {
-        	index index.php index.html index.htm;
+        #index.php index.html index.htm;
                 # First attempt to serve request as file, then
                 # as directory, then fall back to displaying a 404.
                 try_files \$uri \$uri/ =404;
@@ -27,21 +31,23 @@ server {
 }
 EOF
 
+echo "=======Hapus file Html dan clone pesbuk dari github"
 #setup application
 sudo rm -rf /var/www/html/*
 cd /var/www/html && sudo git clone https://github.com/miqbalnawawi/sosial-media.git && sudo mv sosial-media/* .
 
+
+echo "samakan site available dan site enable"
 #samakan site available dan site enable
 sudo ln -s /etc/nginx/sites-available/pesbuk /etc/nginx/sites-enabled/pesbuk
 
+echo "unlink default"
 #unlink default
 sudo unlink /etc/nginx/sites-enabled/default
+sudo unlink /etc/nginx/sites-available/default
 
-#restart service
-sudo nginx -t
-sudo systemctl nginx restart
-sudo systemctl restart php7.2-fpm
 
+echo "==============create database=========="
 #create database
 cd /var/www/html/ 
 sudo mysql -u root << EOF
@@ -51,5 +57,12 @@ GRANT ALL PRIVILEGES ON * . * TO 'iqbal'@'localhost';
 FLUSH PRIVILEGES;   
 EOF
 
+
+echo "samakan database"
 #dump / samakan database
 sudo mysql dbsosmed < dump.sql
+
+echo "=========================restart service====================="
+#restart service
+sudo nginx -t
+sudo systemctl restart nginx
